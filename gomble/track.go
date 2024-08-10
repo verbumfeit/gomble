d1e6b22f13f97afdc5894cebcd7bd91a58a627ef
verbumfeit <verbumfeit@tuta.io>
2024-08-10 05:55:14 +0200

    Fix stopping of tracks Only create a single encoder

--- gomble/track.go
+++ gomble/track.go
@@ -30,6 +30,8 @@ var stop chan bool = make(chan bool)
 var pause chan bool = make(chan bool)
 var currTrack *Track
 
+var enc, _ = audioformats.NewOpusEncoder() // initializes a new encoder
+
 const (
 	TRACK_TYPE_YOUTUBE = iota
 	TRACK_TYPE_OGGFILE
@@ -39,22 +41,20 @@ var yttrackregex *regexp.Regexp = regexp.MustCompile(`https://(www\.)?youtu(be\.
 var ytplaylistregex *regexp.Regexp = regexp.MustCompile(`https://(www\.)?youtube\.com/(playlist\?list=)([a-zA-Z0-9\-\_]+)`)
 
 func LoadTrack(id string) (*Track, error) {
-	var err error
 	var src tracksources.TrackSource
 	if _, err := os.Stat("/usr/bin/yt-dlp"); err == nil {
 		// use youtube-dl if it exists
 		src, err = youtube.NewYoutubedlVideo(id)
 		if err != nil {
 			logger.Errorf("error creating video: %d", err)
+			return nil, err
 		}
 	} else {
 		// otherwise use native youtube stream implementation (probably doesn't work, but worth a try)
 		// src, err = youtube.NewYoutubeVideo(surl)
 		logger.Fatalf("No Youtube-dl installed. exiting\n")
 	}
-	if err != nil {
-		return nil, err
-	}
+
 	return &Track{
 		trackSrc: src,
 	}, nil
@@ -75,16 +75,22 @@ func LoadPlaylist(id string) ([]*Track, error) {
 	// turn command results into slice of ids
 	idstring := string(buf)
 	idlist = strings.Split(idstring, "\n")
-	
-	logger.Infof("Got playlist of length ", strconv.Itoa(len(idlist)))
+	// remove last element, because it is an empty string
+	idlist = idlist[:len(idlist)-1]
 
+	for _, id := range idlist {
+		logger.Infof("ID: %v\n", id)
+	}
+
+	logger.Infof("Got playlist of length %v\n", strconv.Itoa(len(idlist)))
 
 	for _, id := range idlist {
 		track, err := LoadTrack(id)
 		if err != nil {
-			logger.Errorf("Could not load track: ", id)
+			logger.Errorf("Could not load playlist track: %v\n", id)
 			continue
 		}
+		logger.Infof("Loaded track: %v\n", track.trackSrc.GetTitle())
 		tracklist = append(tracklist, track)
 	}
 
@@ -100,16 +106,22 @@ func LoadUrl(url string) ([]*Track, error) {
 	if len(ytplaylistmatches) > 0 {
 		// we have a playlist
 		id := ytplaylistmatches[3]
-		logger.Infof("LISTID:"+id) 
-		tracklist, _ := LoadPlaylist(id)
+		logger.Infof("LISTID: %v\n", id)
+		tracklist, err := LoadPlaylist(id)
+		if err != nil {
+			return nil, err
+		}
 		return tracklist, nil
 	} else if len(yttrackmatches) > 0 {
 		// we have a single track
 		id := yttrackmatches[4]
-		track, _ := LoadTrack(id)
+		track, err := LoadTrack(id)
+		if err != nil {
+			return nil, err
+		}
 		tracklist = append(tracklist, track)
 		return tracklist, nil
-	} 
+	}
 	return nil, errors.New("LoadTrack (track.go): No Youtube Video Found under URL")
 }
 
@@ -123,10 +135,12 @@ func GetCurrentTrack() *Track {
 
 func Play(t *Track) bool { // {{{
 	if t.Done {
+		logger.Debugf("Track is done")
 		return false
 	}
 	if currTrack != nil {
 		Stop()
+		stop <- false
 	}
 	currTrack = t
 	t.buffer_ms = 500 // buffering of 500 ms should be enough, I think...
@@ -150,14 +164,13 @@ func Resume() {
 
 // This audioroutine gets called whenever a new audio stream should be played
 func audioroutine(t *Track) { // {{{
-	enc, err := audioformats.NewOpusEncoder() // initializes a new encoder
-	if err != nil {
-		eventpuffer <- TrackExceptionEvent{
-			Track: t,
-			err:   fmt.Errorf("Could not create Opus Encoder: %v\n", err),
-		}
-		return
-	}
+	// if err != nil {
+	// 	eventpuffer <- TrackExceptionEvent{
+	// 		Track: t,
+	// 		err:   fmt.Errorf("Could not create Opus Encoder: %v\n", err),
+	// 	}
+	// 	return
+	// }
 
 	opusbuf := make(chan []byte, t.buffer_ms/audioformats.OPUS_FRAME_DURATION) // make channel with buffer_ms/frame_duration number of frames as buffer
 	// our producer
@@ -176,10 +189,10 @@ func audioroutine(t *Track) { // {{{
 			}
 			select {
 			case opusbuf <- opusPayload:
-				break
+				continue
 			case <-stop:
 				close(opusbuf)
-				stop <- false
+				// stop <- false
 				return
 			}
 		}
@@ -218,40 +231,40 @@ loop:
 				//TODO Check if pause is true or false (pause or resume)
 			}
 		default:
-		}
-		logger.Debugf("Get next opus frame, number of frames in buffer: %d\n", len(opusbuf))
-		lastTime := time.Now()
-		opusPayload, ok := <-opusbuf
-		elapsed := time.Since(lastTime)
-		if elapsed.Microseconds() > 4000 {
-			logger.Warnf("elapsed time from getting next opus frame: %s\n", elapsed)
-		}
-		if !ok {
-			// Channel got closed -> track is done
-			t.Done = true
-			currTrack = nil
-			eventpuffer <- TrackFinishedEvent{
-				Track: t,
+			logger.Debugf("Get next opus frame, number of frames in buffer: %d\n", len(opusbuf))
+			lastTime := time.Now()
+			opusPayload, ok := <-opusbuf
+			elapsed := time.Since(lastTime)
+			if elapsed.Microseconds() > 4000 {
+				logger.Warnf("elapsed time from getting next opus frame: %s\n", elapsed)
 			}
-			break
-		}
-		if opusPayload == nil {
-			// happens if there was an error getting the opus payload from our producer
-			t.Done = true
-			currTrack = nil
-			eventpuffer <- TrackExceptionEvent{
-				Track: t,
+			if !ok {
+				// Channel got closed -> track is done
+				t.Done = true
+				currTrack = nil
+				eventpuffer <- TrackFinishedEvent{
+					Track: t,
+				}
+				break loop
 			}
-			break
+			if opusPayload == nil {
+				// happens if there was an error getting the opus payload from our producer
+				t.Done = true
+				currTrack = nil
+				eventpuffer <- TrackExceptionEvent{
+					Track: t,
+				}
+				break loop
+			}
+			sendAudioPacket(opusPayload, uint16(len(opusPayload)), last)
+
+			//file.Write(opusPayload)
 		}
-		sendAudioPacket(opusPayload, uint16(len(opusPayload)), last)
+		//WriteInt16InFile("pcm.final", allpcm)
+	} // }}}
+}
 
-		//file.Write(opusPayload)
-	}
-	//WriteInt16InFile("pcm.final", allpcm)
-} // }}}
-
-//var allpcm []int16
+// var allpcm []int16
 func getNextOpusFrame(t *Track, encoder *audioformats.OpusEncoder) ([]byte, error) { // {{{
 	pcm, err := t.trackSrc.GetPCMFrame(audioformats.OPUS_FRAME_DURATION)
 	if err != nil {
